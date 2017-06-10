08bfa2f7619e2e2f005163ff3c90a739294ec21e
strider90arun <strider90arun@gmail.com>
2017-06-09 18:54:55 -0700

    Shoe brands in query

--- SportsService/src/main/java/com/arun/specification/ShoeSpecification.java
+++ SportsService/src/main/java/com/arun/specification/ShoeSpecification.java
@@ -1,9 +1,17 @@
 package com.arun.specification;
 
+import com.arun.common.PathOperation;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.jpa.domain.Specification;
 import com.arun.common.FilteredPaths;
 import com.arun.domain.Shoes;
 import com.arun.domain.Shoes_;
+
+import javax.persistence.criteria.Predicate;
+import java.util.List;
 
 public class ShoeSpecification extends FilteredPaths<Shoes> {
 
@@ -24,6 +32,26 @@ public class ShoeSpecification extends FilteredPaths<Shoes> {
 		return (root, criteriaQuery, criteriaBuilder) -> {
 			return criteriaBuilder.equal(root.get(Shoes_.price), price);
 		};
+	}
+
+	private PathOperation<Shoes> brandPathOperation() {
+		return (cb, path, value) -> {
+            if (StringUtils.isBlank(value)) {
+                return cb.and();
+            }
+            List<Predicate> predicates = Lists.newArrayList();
+            Iterable<String> brands = Splitter.on(SEPARATOR).split(value);
+            List<String> brandsIn = Lists.newArrayList();
+            for (String brand : brands) {
+                if (StringUtils.isNotBlank(brand)) {
+                    brandsIn.add(brand);
+                }
+            }
+            if (CollectionUtils.isNotEmpty(brandsIn)) {
+                predicates.add(path.in(brandsIn));
+            }
+            return cb.and(predicates.toArray(new Predicate[predicates.size()]));
+        };
 	}
 	
 }
