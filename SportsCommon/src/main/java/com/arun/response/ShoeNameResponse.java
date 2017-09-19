f19647412ecb94f613964410640ae5cc40564875
strider90arun <strider90arun@gmail.com>
2017-09-18 23:46:21 -0700

    Code cleanup

--- SportsCommon/src/main/java/com/arun/response/ShoeNameResponse.java
+++ SportsCommon/src/main/java/com/arun/response/ShoeNameResponse.java
@@ -4,31 +4,15 @@ import java.util.List;
 
 import com.arun.dto.ShoeNameDTO;
 
-
-
 public class ShoeNameResponse {
 
 	private List<ShoeNameDTO> shoeNames;
 
-	/**
-	 * @return the shoeNames
-	 */
 	public List<ShoeNameDTO> getShoeNames() {
 		return shoeNames;
 	}
 
-	/**
-	 * @param shoeNames the shoeNames to set
-	 */
 	public void setShoeNames(List<ShoeNameDTO> shoeNames) {
 		this.shoeNames = shoeNames;
 	}
-
-	
-
-	
-
-	
-	
-	
 }
